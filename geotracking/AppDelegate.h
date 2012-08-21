1bffa73b0fb6b21c054ccc6a5c6792abe7303043
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2012-08-21 21:37:48 +0400

    Initial Commit

---
+++ geotracking/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  geotracking
+//
+//  Created by Григорьев Максим on 8/21/12.
+//  Copyright (c) 2012 Maxim V. Grigoriev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ geotracking/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  geotracking
+//
+//  Created by Григорьев Максим on 8/21/12.
+//  Copyright (c) 2012 Maxim V. Grigoriev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
