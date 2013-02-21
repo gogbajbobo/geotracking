7736f698e41992331b4adc24a7506a7c1bd6a23f
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2013-02-21 16:51:13 +0400

    localisation part7

--- geotracking/Classes/STGTFilterSpotViewController.m
+++ geotracking/Classes/STGTFilterSpotViewController.m
@@ -28,13 +28,18 @@
 }
 
 - (void)viewWillAppear:(BOOL)animated {
+    
     for (int i = 0; i < self.viewControllers.count; i++) {
         if ([[self.viewControllers objectAtIndex:i] isKindOfClass:[STGTSpotPropertiesViewController class]]) {
             STGTSpotPropertiesViewController *spvc = (STGTSpotPropertiesViewController *)[self.viewControllers objectAtIndex:i];
             spvc.tracker = self.tracker;
             spvc.spot = self.filterSpot;
             UITabBarItem *tabBarItem = [self.tabBar.items objectAtIndex:i];
-            spvc.typeOfProperty = [tabBarItem.title substringToIndex:(tabBarItem.title.length - 1)];
+            if (tabBarItem.tag == 1) {
+                spvc.typeOfProperty = @"Interest";
+            } else if (tabBarItem.tag == 2) {
+                spvc.typeOfProperty = @"Network";
+            }
 //            NSLog(@"spvc.typeOfProperty %@", spvc.typeOfProperty);
         } else if ([[self.viewControllers objectAtIndex:i] isKindOfClass:[STGTAddressSearchViewController class]]) {
             STGTAddressSearchViewController *asvc = (STGTAddressSearchViewController *)[self.viewControllers objectAtIndex:i];
@@ -49,6 +54,16 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.title = NSLocalizedString(@"FILTER SPOT", @"");
+    UITabBarItem *tabBarItem1 = [self.tabBar.items objectAtIndex:0];
+    tabBarItem1.title = NSLocalizedString(@"INTERESTS 2", @"");
+    tabBarItem1.tag = 1;
+    UITabBarItem *tabBarItem2 = [self.tabBar.items objectAtIndex:1];
+    tabBarItem2.title = NSLocalizedString(@"NETWORKS 2", @"");
+    tabBarItem2.tag = 2;
+    UITabBarItem *tabBarItem3 = [self.tabBar.items objectAtIndex:2];
+    tabBarItem3.title = NSLocalizedString(@"ADDRESS SEARCH", @"");
+    tabBarItem3.tag = 3;
 	// Do any additional setup after loading the view.
 }
 
