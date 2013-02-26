4f31a2c6e7681410c6084edcaf3c9bb2653b670e
kovtash <kovtash@kovtash-mbp.unact.ru>
2013-02-26 11:33:35 +0400

    Комментарии к протоколам

--- geotracking/Protocols/STGTGeoTrackable.h
+++ geotracking/Protocols/STGTGeoTrackable.h
@@ -11,26 +11,34 @@
 #import <CoreLocation/CoreLocation.h>
 #import "STGTDataSyncController.h"
 
+// ========Протокол сессии========
 @protocol STGTSession <NSObject> // не подлежит реализации
-    - (void) initWithUID:(NSString *) uid AuthDelegate(id) authDelegate;
-    - (void) completeSession()
+- (void) initWithUID:(NSString *) uid AuthDelegate(id) authDelegate;
+- (void) completeSession()
 @end
 
+// ========Протоколы менеджера сессий=========
+/* Менеджер сессий - управляет сессиями пользователей и устройств. У каждого пользователя или устройства есть уникальный неизменяемый UID.
+ Менеджер обеспечивает сохдание и асинхронное завершение сесиий. Для каждого UID в каждый момент времени может существовать только одна сессия.
+ */
+
 @protocol STGTSessionManagement <NSObject>
-    - (void) startSessionForUID:(NSString *) uid AuthDelegate(id) authDelegate;
-    - (void) stopCurrentSession;
+- (void) startSessionForUID:(NSString *) uid AuthDelegate(id) authDelegate; //Создание сессии или ее активация, если сессия уже существует. Если в момент старта сессии уже имеется активная сессия, перед стартом новой сессии текущей сессии передается команда на завершение.
+- (void) stopCurrentSession()
 @end
 
 @protocol STGTSessionManager <NSObject>
-    - (void) sessionCompletionFinished:(id) sender;
+- (void) sessionCompletionFinished:(id) sender; //callback - вызываемый сессией после окончания процедуры завершения сессии
 @end
 
+// =======Протокол управляемой сессии==========
 @protocol STGTManagedSession <STGTSession>
-    @property (weak,nonatomic) id <STGTSessionManager> manager;
+@property (weak,nonatomic) id <STGTSessionManager> manager; // ссылка на менеджер сессий, которому будет возвращен callbak sessionCompletionFinished
 @end
 
+// Объект, предназначенный заменить синглтон STGTTrackingLocationController
 @interface STGTSession : NSObject <STGTManagedSession>
-    @property (strong,nonatomic) STGTLocationManagedDocument *locationManagedDocument;
-    @property (strong,nonatomic) CLLocationManager *locationManager;
-    @property (strong,nonatomic) STGTDataSyncController *syncController;
+@property (strong,nonatomic) STGTLocationManagedDocument *locationManagedDocument;
+@property (strong,nonatomic) STGTDataSyncController *syncController;
+@property (strong,nonatomic) CLLocationManager *locationManager;
 @end
