8a7b6169665971825612a4ae77e9ed708ec20449
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2013-03-20 09:34:41 +0400

    new STGT_SYNC_SERVER_URI

--- geotracking/Classes/STGTSettingsController.m
+++ geotracking/Classes/STGTSettingsController.m
@@ -28,7 +28,7 @@
 
 #define STGT_FETCH_LIMIT 20
 #define STGT_SYNC_INTERVAL 1800.0
-#define STGT_SYNC_SERVER_URI @"https://oldcat.unact.ru/rc_unact_old/chest"
+#define STGT_SYNC_SERVER_URI @"https://system.unact.ru/asa/?_host=asa0&_svc=chest"
 #define STGT_XML_NAMESPACE @"https://github.com/sys-team/ASA.chest"
 
 #define STGT_BATTERY_CHECKING NO
